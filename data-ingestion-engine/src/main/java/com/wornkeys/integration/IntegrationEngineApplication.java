6a6104fe42d6c2d768d3eb4a906e85c88774322a
Shailendra Karambe <shailendra@wornkeys.dev>
2026-04-09 20:14:17 +0530

    Apache Camel Data Pipeline

---
+++ data-ingestion-engine/src/main/java/com/wornkeys/integration/IntegrationEngineApplication.java
@@ -0,0 +1,11 @@
+package com.wornkeys.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationEngineApplication.class, args);
+    }
+}
